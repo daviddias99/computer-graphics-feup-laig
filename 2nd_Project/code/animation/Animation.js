8939447ac63fc0370f57d9af4c448f49ca5a9661
David Silva <up201705373@fe.up.pt>
2019-11-24 13:52:02 +0000

    Added comments to animation

--- 2nd_Project/code/animation/Animation.js
+++ 2nd_Project/code/animation/Animation.js
@@ -1,15 +1,32 @@
+/**
+ * Class to represent a generic animation and it's core functionalitties
+ */
 class Animation {
 
+    /** 
+     * @constructor                 Animation constructor
+     * @param {XMLscene} scene      Reference to the scene in which the animation will take place
+     * @param {Number} id           Unique ID of the animation
+     */
     constructor(scene,id) {
         this.id = id;
         this.scene = scene;
-        this.transformationMatrix = mat4.create();
-        this.inUse = false; 
+        this.transformationMatrix = mat4.create();  // Init the transf matrix as the identity matrix (no transformation)
+        this.inUse = false;                         // inUse signals if the animation is currently being used.(used for performance as there is no need to update animations that are not in use)
     }
     
+    /**
+     * @method update
+	 * Updates the animation transformation matrix according to the current time value
+     * @param {Number} t    timeValue
+     */
     update(t) { 
     }
     
+    /**
+     * @method apply
+	 * Applies the animation to the scene
+     */
     apply() {
         this.scene.multMatrix(this.transformationMatrix);
     }
--- 2nd_Project/code/animation/Segment.js
+++ 2nd_Project/code/animation/Segment.js
@@ -1,26 +1,49 @@
+/**
+ * Class representing an animation segment. A segment is bounded by a start and an end keyframe. It's duration is the time between both keyframes and the animation matrix
+ * is the interpolation of the keyframes according to the uorrent instant.
+ */
 class Segment {
 
+    /**
+     * @constructor Builds an animation segment. endKeyFrame.instant must be higher thatn startKeyFrame.instant
+     * @param {Keyframe} startKeyFrame      Keyframe corresponding to the start of the segment
+     * @param {Keyframe} endKeyFrame        Keyframe corresponding to the end of a segment 
+     */
     constructor(startKeyFrame, endKeyFrame){
 
         this.startKeyFrame = startKeyFrame;
         this.endKeyFrame = endKeyFrame;
+
+        // The duration of a segment is the time between the instants of it's bounding keyframes
         this.duration = this.endKeyFrame.getTimeMilli() - this.startKeyFrame.getTimeMilli();
     }
 
 
+    /**
+    * @method interpolateFrames
+	 * Returns the transformation matrix corresponding to the interpolation of start and end Keyframes according to t
+    * @param {Number} t    timeValue
+    */
     interpolateFrames(t){
 
+        // Time since the beginning of the segment
         let deltaT = t  - this.startKeyFrame.getTimeMilli();
 
         let transfMatrix = mat4.create();
 
+        // Interpolate the translation
+
         let translCoords = arrayLinearInterpolation(deltaT,this.duration,0,this.endKeyFrame.translation,this.startKeyFrame.translation);
         transfMatrix = mat4.translate(transfMatrix, transfMatrix, translCoords);
+
+        // Interpolate the rotation
 
         let rotCoords = arrayLinearInterpolation(deltaT,this.duration,0,this.endKeyFrame.rotation,this.startKeyFrame.rotation);
         transfMatrix = mat4.rotateX(transfMatrix, transfMatrix, rotCoords[0]);
         transfMatrix = mat4.rotateY(transfMatrix, transfMatrix, rotCoords[1]);
         transfMatrix = mat4.rotateZ(transfMatrix, transfMatrix, rotCoords[2]);
+
+        // Interpolate the scalling
 
         let scaleCoords = arrayGeometricProgressionTerm(this.startKeyFrame.scale,this.endKeyFrame.scale,this.duration,deltaT);
         transfMatrix = mat4.scale(transfMatrix, transfMatrix, scaleCoords);   
