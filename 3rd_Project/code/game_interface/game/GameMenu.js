9c3c3f8d0699933b61f2326881c6bff3dec5ea5b
David Silva <up201705373@fe.up.pt>
2020-01-05 12:35:53 +0000

    Added documentation for gamemenu

--- 3rd_Project/code/game_interface/game/GameMenu.js
+++ 3rd_Project/code/game_interface/game/GameMenu.js
@@ -1,15 +1,27 @@
+/**
+ * GameMenu is a child class of GameTheme as it is also loaded through a XMl but a game cannot be played in it. Also, it as menu options on the dat.gui interface and 
+ * object interface that are not available in GameTheme.
+ */
 class GameMenu extends GameTheme {
 
     constructor(scene, orchestrator) {
         super('menu.xml', scene, orchestrator);
     }
 
+    /**
+     * @method initInterface
+     * 
+     * Add the board size and player type choosing to the dat.gui interface
+     */
     initInterface() {
         let gui = this.scene.interface.gui;
 
+        const MINBOARDSIZE = 4;
+        const MAXBOARDSIZE = 14;
+
         this.boardFolder = gui.addFolder('Board');
-        this.boardFolder.add(this.orchestrator, 'boardHeight', 4, 14).name('Height').step(1);
-        this.boardFolder.add(this.orchestrator, 'boardWidth', 4, 14).name('Width').step(1);
+        this.boardFolder.add(this.orchestrator, 'boardHeight', MINBOARDSIZE, MAXBOARDSIZE).name('Height').step(1);
+        this.boardFolder.add(this.orchestrator, 'boardWidth', MINBOARDSIZE, MAXBOARDSIZE).name('Width').step(1);
         this.boardFolder.open();
 
         this.playerFolder = gui.addFolder('Player');
@@ -18,6 +30,11 @@ class GameMenu extends GameTheme {
         this.playerFolder.open();
     }
 
+    /**
+     * @method destroyInterface
+     * 
+     * Remove the board size and player type interface from dat.gui
+     */
     destroyInterface() {
         let gui = this.scene.interface.gui;
 
@@ -25,6 +42,12 @@ class GameMenu extends GameTheme {
         gui.removeFolder(this.playerFolder);
     }
 
+
+    /**
+     * @method onGraphLoaded
+     * 
+     * Same as game theme's on graph loaded but adds extra interface elements.
+     */
     onGraphLoaded() {
         super.onGraphLoaded();
         this.scene.interface.setActiveCamera();
