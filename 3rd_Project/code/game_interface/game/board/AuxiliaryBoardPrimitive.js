0366dd50c447767e78e58f11cadf7d14651bc46c
David Silva <up201705373@fe.up.pt>
2020-01-05 15:47:14 +0000

    Added auxiliaryboard primitive documentation

--- 3rd_Project/code/game_interface/game/board/AuxiliaryBoardPrimitive.js
+++ 3rd_Project/code/game_interface/game/board/AuxiliaryBoardPrimitive.js
@@ -1,16 +1,28 @@
-class AuxiliaryBoardPrimitive{
+/**
+ * Class that represents the auxiliary-board casing. 
+ */
+class AuxiliaryBoardPrimitive {
 
-    constructor(scene,rows,boardPrimitive,type,material){
+    /**
+     * @constructor
+     * 
+     * @param {CGFscene}            scene           scene
+     * @param {number}              rows            number of rows of the board
+     * @param {BoardPrimitive}      boardPrimitive  used for getting location information
+     * @param {number}              type            -1 if player 1, 1 if player 2         
+     * @param {material}            material        material used in the auxiliary board
+     */
+    constructor(scene, rows, boardPrimitive, type, material) {
 
         this.scene = scene;
         this.aux_board_primitive_side = new Prism(this.scene, 4);
         this.aux_board_primitive_top = new MyPlane(this.scene, 10, 10);
         this.boardPrimitive = boardPrimitive;
-        this.arr = this.boardPrimitive.getAuxBoardDisplayParameters();
+        this.displayParameters = this.boardPrimitive.getAuxBoardDisplayParameters();
         this.type = type;
         this.material = material;
-        
-        if(type == -1){
+
+        if (type == -1) {
             this.addition = 0;
         }
         else {
@@ -18,34 +30,51 @@ class AuxiliaryBoardPrimitive{
         }
     }
 
+    /**
+    * @method changeMaterials
+    * 
+    * Change the materialof the auxiliary board casing
+    * @param {Array} material 
+    */
     changeMaterials(material) {
         this.material = material;
     }
 
-    display(){
+    /**
+     * @method display
+     * 
+     * Display the auxiliary board casing
+     */
+    display() {
 
-        let auxBoardPosition = [0,this.boardPrimitive.board_height/2, this.arr[0] + (this.type * 1.37 + this.addition) * this.arr[1]];
+        let auxBoardPosition = [0, this.boardPrimitive.board_height / 2, this.displayParameters[0] + (this.type * 1.37 + this.addition) * this.displayParameters[1]];
 
         this.material.apply();
-        this.scene.pushMatrix();        
-
-        this.scene.pushMatrix();  
-        this.scene.translate(...auxBoardPosition);
-        this.scene.scale(...this.arr[2]);
-        this.scene.translate(0.5,0,0);
+        this.scene.pushMatrix();
 
         this.scene.pushMatrix();
-        this.scene.translate(0.0,this.boardPrimitive.board_height/2,0);
+
+        // Scale and position the aux board
+        this.scene.translate(...auxBoardPosition);
+        this.scene.scale(...this.displayParameters[2]);
+        this.scene.translate(0.5, 0, 0);
+
+        // Display the top of the aux board
+
+        this.scene.pushMatrix();
+        this.scene.translate(0.0, this.boardPrimitive.board_height / 2, 0);
         this.aux_board_primitive_top.display();
         this.scene.popMatrix();
-        
+
+        // Display the bottom of the aux board
         this.scene.pushMatrix();
-        this.scene.translate(0.0,-this.boardPrimitive.board_height/2,0);
-        this.scene.rotate(Math.PI,1,0,0);
+        this.scene.translate(0.0, -this.boardPrimitive.board_height / 2, 0);
+        this.scene.rotate(Math.PI, 1, 0, 0);
         this.aux_board_primitive_top.display()
         this.scene.popMatrix();
-        
-        this.scene.translate(0.0, -this.boardPrimitive.board_height/2,0);
+
+        // Display the sides the aux board
+        this.scene.translate(0.0, -this.boardPrimitive.board_height / 2, 0);
         this.scene.rotate(Math.PI / 4.0, 0.0, 1.0, 0.0);
         this.scene.scale(Math.sqrt(0.5), this.boardPrimitive.board_height, Math.sqrt(0.5));
         this.aux_board_primitive_side.display();
