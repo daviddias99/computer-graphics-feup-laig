9da0f55f40756efdd3209ed0db52e4f6865625ff
David Silva <up201705373@fe.up.pt>
2020-01-05 13:15:00 +0000

    Added documentation for Gamestate

--- 3rd_Project/code/game_interface/game/GameState.js
+++ 3rd_Project/code/game_interface/game/GameState.js
@@ -1,77 +1,112 @@
+/**
+ * Class that represents a gamestate of the game Squex.
+ */
 class GameState {
 
-    constructor(boardMatrix, p1Type, p2Type, nextPlayer, cutInfo){
+    constructor(boardMatrix, p1Type, p2Type, nextPlayer, cutInfo) {
 
+        // For information about the meaning of these elements consult the prolog implementation
         this.boardMatrix = boardMatrix;
         this.p1Type = p1Type;
         this.p2Type = p2Type;
         this.nextPlayer = nextPlayer;
         this.cutInfo = cutInfo;
+
+        // These parameters are extra from the prolog implementation. Previous move represents the move that lead to this gamestate and next move the move that
+        // was played after this gamestate
         this.previousMove = null;
         this.nextMove = null;
 
     }
 
-    getNextPlayerType(){
+    /**
+     * @method getNextPlayerType
+     * 
+     * Get the type of the next player. Possible values are 'P' for player and 1 or 2 for levels 1-2 bot
+     */
+    getNextPlayerType() {
 
-        if(this.nextPlayer == 1){
+        if (this.nextPlayer == 1) {
 
             return this.p1Type;
         }
         else {
             return this.p2Type;
         }
-
-
     }
 
-    botCount(){
+    /**
+     * @methods botCount
+     * 
+     * Get the number of bot players that are in the game. May be 0, 1 or 2
+     */
+    botCount() {
 
         let count = 0;
 
-        if(this.p1Type != 'P')
+        if (this.p1Type != 'P')
             count++;
-        
-        if(this.p2Type != 'P')
+
+        if (this.p2Type != 'P')
             count++;
 
         return count;
     }
 
-    getHumanPlayerNumber(){
+    /**
+     * @method getHumanPlayerNumber
+     * 
+     * Return the number of the player that is Human. Might be 1 or 2. This function is only meant to be used in a Player v. Bot game, in any other case
+     * it will return false.
+     */
+    getHumanPlayerNumber() {
 
-        if(this.botCount() != 1)
+        if (this.botCount() != 1)
             return false;
 
-        if(this.p1Type == 'P')
+        if (this.p1Type == 'P')
             return 1;
         else
             return 2;
 
     }
 
-    static findMove(previousGs, nextGs){
+    /**
+     * @function findMove Find what Move (see class Move) corresponds to these two consecutive gamestates (nextMove of previousGs and previousMove of nextGs).
+     * @param {GameState} previousGs 
+     * @param {GameState} nextGs 
+     */
+    static findMove(previousGs, nextGs) {
 
         let prevOctBoard = previousGs.boardMatrix['octagonBoard'];
         let nextOctBoard = nextGs.boardMatrix['octagonBoard'];
 
-        for(let i = 0; i < prevOctBoard.length; i++){
 
-            for(let j = 0; j < prevOctBoard[0].length; j++){
-            
-                if(prevOctBoard[i][j] != nextOctBoard[i][j]){
-                    return new Move(j,i);
+        // The move will be the cell of the oct board where the two gamestates differ as only one cell of the octagon board may change between consecutive states.
+
+        for (let i = 0; i < prevOctBoard.length; i++) {
+
+            for (let j = 0; j < prevOctBoard[0].length; j++) {
+
+                if (prevOctBoard[i][j] != nextOctBoard[i][j]) {
+                    return new Move(j, i);
                 }
             }
         }
 
     }
 
+    /**
+     * Build the board matrix from a Board class.
+     * @param {Board} board 
+     */
     buildMatrixFromBoard(board) {
 
         this.boardMatrix = [];
         let octagonBoard = [];
         let squareBoard = [];
+
+        // Find what octagons have a piece.
 
         for (let i = 0; i < board.octagons.length; i++) {
 
@@ -86,6 +121,8 @@ class GameState {
 
             octagonBoard[i] = row;
         }
+
+        // Find what squares have a piece.
 
         for (let i = 0; i < board.squares.length; i++) {
 
