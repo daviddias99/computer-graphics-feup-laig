3a68535d91b1be62b7d208da27d42770d1cfaefc
David Silva <up201705373@fe.up.pt>
2020-01-05 14:41:37 +0000

    Added piece documentation

--- 3rd_Project/code/game_interface/game/board/Piece.js
+++ 3rd_Project/code/game_interface/game/board/Piece.js
@@ -1,10 +1,23 @@
+/**
+ * Class that represents a piece of the game squex (octagon or square). 
+ */
 class Piece {
 
+    /**
+     * @constructor 
+     * @param {CGFobject} primitive     primitive representation of the piece
+     * @param {number} player           player id
+     */
     constructor(primitive, player) {
         this.primitive = primitive;
         this.player = player;
     }
 
+    /**
+     * @method display
+     * 
+     * Display the piece primitive
+     */
     display() {
         this.primitive.display(this.player)
     }
