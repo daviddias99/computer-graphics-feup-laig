0911ed2a0e275aa654a280326675803e86067962
Bernardo Santos <bernardosantos670@gmail.com>
2020-01-05 19:38:39 +0000

    Documented GameOverlayElement.js

--- 3rd_Project/code/game_interface/game/GameOverlayElement.js
+++ 3rd_Project/code/game_interface/game/GameOverlayElement.js
@@ -1,31 +1,38 @@
+/**
+ * Class that represents an overlay element
+ */
 class GameOverlayElement extends CGFobject{
 
     /**
-     * 
-     * @param {XMLScene} scene 
+     * @constructor                 GameOverlayElement constructor
+     * @param {XMLScene} scene      Reference to the scene in which the overlay will be displayed
+     * @param {Array} position      Positioning of the overlay
+     * @param {CGFtexture} texture  Texture that will be applied to the object
      */
     constructor(scene, position, texture){
         super(scene);
-
+        this.elementTex = texture;
         this.element = new MyRectangle(scene,"overlay_element",...position);
-        
-        this.changeTexture(texture);
-
-        // GLSL shader that changes the rectangle so be placed as an overlay and applies the texture 
-        // this.elementShader = new CGFshader(this.scene.gl, 'shaders/overlay_shader.vert', 'shaders/overlay_shader.frag');
     }
 
+    /**
+     * @method changeTexture
+     * @param {CGFtexture} texture 
+     * 
+     * Change the texture that will be applied to the object
+     */
     changeTexture(texture) {
         this.elementTex = texture;
     }
 
-    display(){
-        // this.scene.gl.disable(this.scene.gl.DEPTH_TEST);
-        // this.scene.setActiveShader(this.elementShader);
+    /**
+     * @method display
+     * 
+     * Display the object
+     */
+    display() {
         this.elementTex.bind(0);
         this.element.display();
-        // this.scene.setActiveShader(this.scene.defaultShader);
-        // this.scene.gl.enable(this.scene.gl.DEPTH_TEST);
     }
 
 }
