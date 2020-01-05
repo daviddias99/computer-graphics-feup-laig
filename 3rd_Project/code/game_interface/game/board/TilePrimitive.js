0f5feee64f6285b243093a7500bb0e508c9d071f
David Silva <up201705373@fe.up.pt>
2020-01-05 13:41:44 +0000

    Added comments to tile primitive

--- 3rd_Project/code/game_interface/game/board/TilePrimitive.js
+++ 3rd_Project/code/game_interface/game/board/TilePrimitive.js
@@ -1,33 +1,58 @@
+/**
+ * Class representing a primitive of a game tile (in the case of squex, a octagon or a square)
+ */
 class TilePrimitive extends CGFobject {
 
+    /**
+     * @constructor
+     * 
+     * Create a new tile primitive with the given parameters
+     * 
+     * @param {CGFScene} scene                  scene             
+     * @param {float} radius                    radius of the tile
+     * @param {Integer} sides                   number of tiles of the tile (in squex 4 or 8)
+     * @param {CGFappearence} material          material to be applied to the tile
+     */
     constructor(scene, radius, sides, material) {
         super(scene);
 
         this.radius = radius;
         this.sides = sides;
         this.material = material;
-
         this.tile = new Poligon(this.scene, this.sides);
     }
 
+    /**
+     * @method changeMaterials
+     * 
+     * Change the material of the tile to a new one
+     * @param {CGFappearence} material 
+     */
     changeMaterials(material) {
         this.material = material;
     }
 
+    /**
+     * @method getRadius
+     * 
+     * Get the radius of the tile
+     */
     getRadius() {
         return this.radius;
     }
 
+    /**
+     * @method display
+     * 
+     * Display the tile with the correct material and size.
+     */
     display() {
         this.material.apply();
 
         this.scene.pushMatrix();
         this.scene.scale(this.radius, 1, this.radius);
 
-        if (this.sides == 8)
-            this.tile.display();
-        else
-            this.tile.display();
+        this.tile.display();
 
         this.scene.popMatrix();
     }
