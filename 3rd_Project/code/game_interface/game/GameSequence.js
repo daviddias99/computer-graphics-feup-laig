c99bad0d66098ec79fc4dde5fce3c8a9bcaec273
David Silva <up201705373@fe.up.pt>
2020-01-05 13:33:32 +0000

    Added documentation for GameSequence

--- 3rd_Project/code/game_interface/game/GameSequence.js
+++ 3rd_Project/code/game_interface/game/GameSequence.js
@@ -1,12 +1,29 @@
+/**
+ * Class representing the sequence of gamestates that are playing throughout a game.
+ */
 class GameSequence {
     
+    /**
+     * @constructor 
+     * 
+     * Starts a new GameSequence with an initial gamestate (usually this gamestate is the initial gamestate of a game).
+     * 
+     * @param {GameState} initialGamestate 
+     */
     constructor(initialGamestate){
 
-        this.states = [initialGamestate];
+        this.states = [initialGamestate];   // The start of beggining of the array represents the most recent gamestate of the game
         this.currentMovieState = 0;
-        this.inMovie = false;
+        this.inMovie = false;               // Signals if the gamesequence is currently being used to display the game movie
     }
 
+    /**
+     * @method addGamestate
+     * 
+     * Adds a new gamestate to the sequence. Updates the nextMove variable of the added gamestate.
+     * 
+     * @param {GameState} gamestate new gamestate
+     */
     addGamestate(gamestate){
 
         this.states[0].nextMove = gamestate.previousMove;
@@ -14,21 +31,43 @@ class GameSequence {
     }
 
 
+    /**
+     * @method getInitialGamestate
+     * 
+     * Get the first gamestate of the game
+     */
     getInitialGamestate(){
         return this.states[this.states.length  -1];
     }
 
+    /**
+     * @method getCurrentGamestate
+     * 
+     * Get the current gamestate of the game
+     */
     getCurrentGamestate(){
 
         return this.states[0];
     }
 
+    /**
+     * @method getPreviousGamestate
+     * 
+     * Get the previous gamestate of the game. If the game only as seen one gamestate, the function will return undefined
+     */
     getPreviousGamestate(){
 
         if(this.states.length > 1)
             return this.states[1];
     }
 
+    /**
+     * @method undo
+     * 
+     * Roll back to the previous gamestate. Undo is only possible if there is at least 2 gamestates in a game. The initial gamestate cannot be undone.
+     * 
+     * @return true if the undo was sucessfull, false otherwise
+     */
     undo(){
 
         if(this.states.length > 1) {
@@ -43,11 +82,24 @@ class GameSequence {
 
     }
 
+    /**
+     * @method getMovieGamestate
+     * 
+     * Return the current movie gamestate.
+     */
     getMovieGamestate(){
 
         return this.states[this.currentMovieState];
     }
 
+    /**
+     * @method stepMovieGamestate
+     * 
+     * Advance the movie to the next gamestate in the sequence. This method only works if the Movie as started with the startMovie method. If the movie ended,
+     * the method will change the inMovie variable accordingly.
+     * 
+     * @return true if the operation was sucessfull, false otherwise.
+     */
     stepMovieGamestate(){
 
         if(!this.inMovie)
@@ -55,6 +107,7 @@ class GameSequence {
 
         this.currentMovieState--;
 
+        // The movie reached the current state
         if(this.currentMovieState == 0){
 
             this.inMovie = false;
@@ -63,6 +116,13 @@ class GameSequence {
         return true;
     }
 
+    /**
+     * @method startMovie
+     * 
+     * Start the movie processing in the sequence. Set the inMovie variable to true.
+     * 
+     * @return true if the movie was started sucessfuly, false otherwise
+     */
     startMovie(){
 
         if(this.states.length == 1)
