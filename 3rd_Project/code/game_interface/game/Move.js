515128c089d80e2b3484d4614eda95ce96db63f2
David Silva <up201705373@fe.up.pt>
2020-01-05 12:58:08 +0000

    Added Move documentation

--- 3rd_Project/code/game_interface/game/Move.js
+++ 3rd_Project/code/game_interface/game/Move.js
@@ -1,3 +1,6 @@
+/**
+ * Classe Move represents a game move that as an horizontal coordinate and a vertical coordinate 
+ */
 class Move {
 
     constructor(x,y){
@@ -5,6 +8,11 @@ class Move {
         this.y = y;
     }
 
+    /**
+     * @method toString
+     * 
+     * Returns the move in the format xCoord-yCoord
+     */
     toString(){
 
         return this.x + "-" + this.y;
